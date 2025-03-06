1ba7a929f6b48b9ce8d2f07a751301e9cb8d0253
RCOM363 <rahullankeppanavar363@gmail.com>
2025-03-06 08:49:52 +0000

    add signup page

--- src/app/api/check-username/route.ts
+++ src/app/api/check-username/route.ts
@@ -55,7 +55,7 @@ export async function GET(request: Request) {
     return Response.json(
       {
         success: true,
-        message: "Username is available",
+        message: "Username is unique",
       },
       {
         status: 200,
