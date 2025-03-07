c540897d081501a8229ca581a342a1709bdd2d37
RCOM363 <rahullankeppanavar363@gmail.com>
2025-03-07 13:01:28 +0000

    feat: add delete message API

---
+++ src/app/api/delete-message/[messageId]/route.ts
@@ -0,0 +1,70 @@
+import { getServerSession } from "next-auth";
+import mongoose from "mongoose";
+import { User as nextAuthUser } from "next-auth";
+
+import dbConnect from "@/lib/dbConnect";
+import User from "@/model/user.model";
+import { authOptions } from "../../auth/[...nextauth]/options";
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { messageId: string } }
+) {
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+  const user: nextAuthUser = session?.user as nextAuthUser;
+
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not authenticated",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  const userId = new mongoose.Types.ObjectId(user._id);
+  const messageId = await params.messageId;
+
+  try {
+    const updatedResult = await User.updateOne(
+      { _id: userId },
+      { $pull: { messages: { _id: messageId } } }
+    );
+
+    if (updatedResult.modifiedCount == 0) {
+      return Response.json(
+        {
+          success: false,
+          message: "Message not found or already deleted",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "Message deleted sucessfully",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.error("Error while deleting message: ", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error while deleting message",
+      },
+      { status: 500 }
+    );
+  }
+}
